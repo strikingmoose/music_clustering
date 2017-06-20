3174b59bbbded65aaf4ad75ae026eed7b4783fbf
chiwang <cwang55@gmail.com>
2017-06-20 12:24:39 -0600

    Implemented multiprocessing and output of data to CSV file

--- load_song_and_extract_features.py
+++ load_song_and_extract_features.py
@@ -14,19 +14,26 @@
 import numpy as np
 import pandas as pd
 import os
-import multiprocessing
+from multiprocessing import Process, JoinableQueue
+import traceback
+import sys
 
 # Load librosa for audio processing
-import librosa
+from librosa import load, feature
 
 # Load eyed3 to extract mp3 metadata
 import eyed3
 
 ##################################################
-# Set constants
+# Set constants and special vars
 ##################################################
 # iTunes directory to loop through
 itunes_dir = r'/Users/chiwang/Documents/iTunes 20160601 copy/iTunes Media/Music/'
+
+# Number of workers to parallelize with multiprocessing
+num_workers = 20
+q = JoinableQueue()
+final_results_list = []
 
 ##################################################
 # class Song represents each mp3 being analyzed
@@ -63,20 +70,86 @@ class Song:
 
     def librosa_mfcc_extract(self, file_path):
         # Load librosa object (only load 30 seconds of the song from 0:45 - 1:15)
-        y, sr = librosa.load(file_path, offset = 45, duration = 30)
+        y, sr = load(file_path, offset = 45, duration = 30)
 
         # Extract MFCC and format to be a single array of features (each MFCC is returned as an array, so we get an
         #   array of arrays where we only want a single array)
-        mfcc = librosa.feature.mfcc(y = y, n_mfcc = 15, fmax = 8000)
-        mfcc_flat = [item for sublist in mfcc for item in sublist]
+        mfcc = feature.mfcc(y = y, n_mfcc = 15, fmax = 8000)
+        mfcc_flat = flatten_list(mfcc)
 
         # Return flat list of MFCCs
         return mfcc_flat
 
+##################################################
+# Define simple utility functions
+##################################################
+def flatten_list(list):
+    return [item for sublist in list for item in sublist]
+
+##################################################
+# Set up queueing and multiprocessing functionality, create worker function processor_job to extract features
+##################################################
+# Define worker function that generates features of a single song, the multiprocessing engine will feed this worker
+#   with songs to analyze in parallel
+def processor_job(worker):
+    # Retrieve file path from the worker
+    file_path = worker[0]
+
+    # Create Song object
+    song = Song(file_path)
+
+    # When extracting the MFCC, we first check whether or not the song is longer than 2 minutes (stored in the
+    #   Song.flag_to_analyze attribute
+    if song.flag_to_analyze:
+        song_result = [song.mp3_title, song.mp3_artist, song.mp3_genre, song.mp3_length]
+        song_result.extend(song.mfcc_flat)
+        final_results_list.append(song_result)
+
+# Define a processor function which manages how many songs to analyze at once by pulling from the queue and processing
+#   it via processor_job
+def processor():
+    while True:
+        # Get a worker from the queue
+        worker = q.get()
+        file = worker[1]
+        current_song_counter = worker[2]
+        total_songs = worker[3]
+
+        # Run the job with the available worker in the queue
+        try:
+            print 'Analyzing song #{} / {}: {}'.format(current_song_counter, total_songs, file)
+            processor_job(worker)
+        except Exception as e:
+            traceback.print_exc(file = sys.stdout)
+        finally:
+            # Job complete
+            # print 'Finished analyzing {}'.format(file_path)
+            q.task_done()
+
+# Kick off processors of workers
+for x in range(num_workers):
+    p = Process(target = processor)
+
+    # Classify process as a daemon so it dies when the main thread dies
+    p.daemon = True
+
+    # Processor goes into effect
+    p.start()
 
 ##################################################
 # Loop through iTunes library
 ##################################################
+# Loop through iTunes library once to gather total number of songs for our reference
+total_songs = 0
+for root, dirs, files in os.walk(itunes_dir):
+    for file in files:
+        # Only look at mp3 files
+        if file[-3:] == 'mp3':
+            # Add 1 song to total_songs
+            total_songs += 1
+
+# Define main function which iterates through iTunes library and adds songs to the multiprocessing queue
+current_song_counter = 1
 for root, dirs, files in os.walk(itunes_dir):
     for file in files:
         # Format file path
@@ -84,6 +157,15 @@ for root, dirs, files in os.walk(itunes_dir):
 
         # Only look at mp3 files
         if file_path[-3:] == 'mp3':
-            # Create Song object
-            song = Song(file_path)
-            print song.mp3_artist, song.mp3_title, song.mp3_genre, song.mp3_length, len(song.mfcc_flat)
+            # Start multiprocessing and feed file path of song to worker
+            q.put([file_path, file, current_song_counter, total_songs])
+            current_song_counter += 1
+
+# Wait until all processes terminate before terminating main
+q.join()
+
+##################################################
+# Save final results
+##################################################
+# Convert results table (list of lists) into pandas dataframe
+pd.DataFrame(final_results_list).to_csv('features.csv')
